e39fd9cab89a0b5e415d94eeee626ef4a5a2495c
Unknown <rducros34@gmail.com>
2018-11-08 10:07:32 +0100

    Refacto to Node step 2

--- KdruClient/app.js
+++ KdruClient/app.js
@@ -1,3 +1,9 @@
+// Module import by npm
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const _ = require('lodash');
+// const pug = require('pug');
 
 // Module instance
 const app = express();
@@ -13,14 +19,24 @@ router.use(bodyParser.json());
 router.use('/css', express.static(`${__dirname}/views/css`));
 router.use('/js', express.static(`${__dirname}/views/js`));
 router.use('/data', express.static(`${__dirname}/views/data`));
+router.use('/img', express.static(`${__dirname}/views/img`));
 
 // path de redirection
 // Path /
 router.get('/', (req, res) => {
-    fs.readFile('./views/home.html', 'utf-8', (error, content) => {
+    fs.readFile('./views/app.html', 'utf-8', (error, content) => {
       res.writeHead(200, {
         'Content-Type': 'text/html',
       });
       res.end(content);
     });
   });
+
+
+// mount the router on the app
+app.use('/', router);
+
+// define listening port of server
+server.listen(3000);
+console.log('App Launched on post 3000 ');
+
